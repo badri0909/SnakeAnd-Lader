0558f20e92961a403b7bda4e879d788f4526e013
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-09 09:18:58 +0530

    add Options.cs

---
+++ Options.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public class Options
+    {
+        ///<summary>
+        /// UC1- Options a rolled dice ca choose any one .
+        ///</summary>
+        public static void ChooseOptions()
+        {
+            Random opt = new Random();
+            string[] options = { "No Play", "Ladder", "Snake" };
+            int oIndex = opt.Next(options.Length);
+
+            Console.WriteLine("Option choosed: {0} ", options[oIndex]);
+
+        }
+    }
+}
