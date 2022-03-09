a6ba1099fb269906e7f1384ea5632bf057637f22
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-09 09:12:24 +0530

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SnakeAndLadderProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake And Ladder\n");
+
+            //StartGame.StartTheGame();
+
+            //RollingDice.DiceRoll();
+
+            Options.ChooseOptions();
+        }
+    }
+}
