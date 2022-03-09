6c66a2043b684f3c1e9854ffb0bb234a61c426fd
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-09 09:16:32 +0530

    add RollingDice.cs

---
+++ RollingDice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public class RollingDice
+    {
+        ///<summary>
+        /// UC2- Dice is Rolled.
+        ///</summary>
+
+        public static void DiceRoll()
+        {
+            Random dice = new Random();
+            int roll = dice.Next(1, 7);
+            Console.WriteLine("Number After Rolling : " + roll);
+        }
+    }
+}
