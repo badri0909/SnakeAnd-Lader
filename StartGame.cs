6111aabadd6f3a339730a42ddbbb0ac9cad55b79
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-09 09:14:42 +0530

    add StartGame.cs

---
+++ StartGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public class StartGame
+    {
+        ///<summary>
+        /// UC1- Player Starts at Zero.
+        ///</summary>
+        public static void StartTheGame()
+        {
+            int start = 0;
+            Console.WriteLine("Player Starts at Zero");
+        }
+    }
+}
